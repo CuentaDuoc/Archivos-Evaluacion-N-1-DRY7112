bc602cd615c0b8058ca6a925e8e2d773eed16fcb
CuentaDuoc <os.placencia@duocuc.cl>
2024-04-13 02:37:27 +0000

    Evaluación N°1 DRY7122

---
+++ Solicitud de informacion.py
@@ -0,0 +1,9 @@
+Nombre=input("Por favor, Ingrese su nombre: ")
+Apellido=input("Por favor, Ingrese su apellido: ")
+Codigodelaseccion=input("Porfavor ingrese el codigo de la seccion sin el guion como, PY2123: ")
+sede=input("Ingrese el nombre de su sede: ")
+
+informacioncomp= f"{Nombre} {Apellido}, Su codigo de la seccion es: {Codigodelaseccion} y su Sede :{sede}"
+
+print ("Su informacion completa es")
+print (informacioncomp)
